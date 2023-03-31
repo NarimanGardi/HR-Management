f451194a7b48501ab6a5d299e874f3fb77ad002f
Nariman Muhsin <narimanmuhsin0@gmail.com>
2023-04-01 02:39:25 +0300

    Error Exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,17 @@
 
 namespace App\Exceptions;
 
+use ErrorException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Throwable;
-
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 class Handler extends ExceptionHandler
 {
     /**
@@ -23,7 +30,17 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+        HttpResponseException::class,
+        AuthorizationException::class,
+        AuthenticationException::class,
+        ValidationException::class,
+        ModelNotFoundException::class,
+        NotFoundHttpException::class,
+        QueryException::class,
+        ThrottleRequestsException::class,
+        ErrorException::class,
+        \Exception::class,
+
     ];
 
     /**
@@ -40,14 +57,48 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public function render($request, $exception)
     {
-        $this->renderable(function (NotFoundHttpException $e, $request) {
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'message' => 'Data not found.'
-                ], 404);
-            }
-        });
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(['error' => 'Resource not found'], 404);
+        }
+
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+
+            return response()->json([
+                'errors' => $errors,
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json(['error' => 'Endpoint not found'], 404);
+        }
+
+        if ($exception instanceof QueryException) {
+            return response()->json(['error' => 'Database error'], 500);
+        }
+
+        if ($exception instanceof ThrottleRequestsException) {
+            return response()->json(['error' => 'Too many requests'], 429);
+        }
+
+        if ($exception instanceof ErrorException) {
+            return response()->json(['error' => 'Internal server error'], 500);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        if($exception instanceof \Exception) {
+            return response()->json(['error' => 'Something went wrong'], 400);
+        }
+
+        return parent::render($request, $exception);
     }
 }
