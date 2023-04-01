de2a2e96f8887ba8982bb274214b85dbdd2ca7b4
Nariman Muhsin <narimanmuhsin0@gmail.com>
2023-04-01 21:55:07 +0300

    removing default user api route

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
     Route::resource('jobs', JobsController::class)->except(['create', 'edit']);
     Route::get('/employees/{id}/managers', [EmployeeController::class, 'getManagers'])->name('api.employees.managers');
     Route::get('/employees/{id}/managers-salary', [EmployeeController::class, 'getManagerSalary']);
